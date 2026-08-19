10080e05997e9a4251b4d9843010b0769beca849
budiantoaja331-cpu <budiantoaja331@gmail.com>
2026-08-19 20:40:46 +0800

    build: refactor build configuration and upgrade Gradle
    - Update Gradle wrapper to 8.10.2
    - Disable configuration cache
    - Add missing Android Kotlin plugin
    - Standardize signing environment variables
    - Remove unused KSP dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,8 +2,8 @@ import com.google.gms.googleservices.GoogleServicesPlugin.MissingGoogleServicesS
 
 plugins {
   alias(libs.plugins.android.application)
+  alias(libs.plugins.kotlin.android)
   alias(libs.plugins.kotlin.compose)
-  alias(libs.plugins.google.devtools.ksp)
   alias(libs.plugins.roborazzi)
   alias(libs.plugins.secrets)
   alias(libs.plugins.google.services)
@@ -51,6 +51,9 @@ android {
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
+  }
+  kotlinOptions {
+    jvmTarget = "11"
   }
   buildFeatures {
     compose = true
@@ -134,6 +137,4 @@ dependencies {
   androidTestImplementation(libs.androidx.runner)
   debugImplementation(libs.androidx.compose.ui.test.manifest)
   debugImplementation(libs.androidx.compose.ui.tooling)
-  "ksp"(libs.androidx.room.compiler)
-  "ksp"(libs.moshi.kotlin.codegen)
 }
